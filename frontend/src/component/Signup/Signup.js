7b38278532d21f94d93e52ae8f42c026c07923e1
prevegimart <pre.vegimart@gmail.com>
2026-01-11 02:17:37 +0530

    Update Signup.js

--- frontend/src/component/Signup/Signup.js
+++ frontend/src/component/Signup/Signup.js
@@ -16,7 +16,7 @@ export default function Signup() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("https://vegimart-back-fnli.onrender.com/api/auth/signup", form);
+      const res = await axios.post("http://localhost:5000/api/auth/signup", form);
       alert("Signup successful");
       navigate("/");
     } catch (err) {
